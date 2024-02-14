82ea4999b945b824810cd7a603f10602391386cc
unknown <nsaghakingsly@gmail.com>
2024-02-14 05:16:06 +0100

    added the build folder

---
+++ public/build/assets/App-jM7K3n2P.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,r as t,o as n}from"./app-CYNvKLER.js";const c={name:"App"};function s(p,a,_,f,i,m){const e=t("router-view");return n(),r(e)}const d=o(c,[["render",s]]);export{d as default};
